2cd5eee0b9a4eeae097ca93498975d9dc13ee9e4
Suraj SK <surajsk2002@gmail.com>
2022-05-20 00:09:46 +0530

    Commit 3

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col items-center justify-content-center h-screen'>
+        <header className='flex w-full p-5 justify-between text-sm text-grey-700'>
+            <div className="flex space-x-4 items-center">
+                <p className='link'>About</p>
+                <p className="link">Store</p>
+            </div>
+
+            <div className="flex space-x-4 items-center">
+                <p className='link'>Gmail</p>
+                <p className="link">Images</p>
+            </div>
+        </header>
+    </div>
   )
 }
 

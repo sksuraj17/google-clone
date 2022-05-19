8262ae6dd3343bc4f7c187a9380b1c38a48bbcc0
Suraj SK <surajsk2002@gmail.com>
2022-05-20 00:21:03 +0530

    Commit 4

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {ViewGridIcon} from "@heroicons/react/solid";
 
 const Home = () => {
   return (
@@ -12,7 +13,8 @@ const Home = () => {
             <div className="flex space-x-4 items-center">
                 <p className='link'>Gmail</p>
                 <p className="link">Images</p>
-            </div>
+                <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" /> 
+                </div>
         </header>
     </div>
   )
